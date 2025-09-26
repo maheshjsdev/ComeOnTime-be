57afc0915f9c4172d1ffd3deaeb21cfee97e2015
maheshjsdev <iammahesh062@gmail.com>
2025-09-27 00:41:41 +0530

    db.js file update

--- config/db.js
+++ config/db.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
 
 const getMongoUri = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return process.env.MONGODB_URI_PROD || process.env.MONGODB_URI;
-  }
-  return process.env.MONGODB_URI_DEV || process.env.MONGODB_URI;
+    //   if (process.env.NODE_ENV === 'production') {
+    //     return process.env.MONGODB_URI_PROD || process.env.MONGODB_URI;
+    //   }
+    console.log("NODE_ENV:", process.env.NODE_ENV);
+    console.log("MONGODB_URI_PROD:", process.env.MONGODB_URI_PROD);
+    console.log("MONGODB_URI_DEV:", process.env.MONGODB_URI_DEV);
+    return process.env.MONGODB_URI_PROD || process.env.MONGODB_URI_DEV;
 };
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(getMongoUri(), {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(getMongoUri(), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
